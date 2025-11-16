66c4b892238a7a685982d75b34ca7f3e8f90f6e1
devflu999 <pmnAQWespagnol10>
2025-11-16 16:48:38 +0100

    feat: Add dotenv configuration

--- src/main/java/com/example/questionapp/QuestionappApplication.java
+++ src/main/java/com/example/questionapp/QuestionappApplication.java
@@ -1,5 +1,6 @@
 package com.example.questionapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuestionappApplication {
 
 	public static void main(String[] args) {
+		// Umgebungsvariablen aus der .env-Datei laden
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> 
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+		
 		SpringApplication.run(QuestionappApplication.class, args);
 	}
 
